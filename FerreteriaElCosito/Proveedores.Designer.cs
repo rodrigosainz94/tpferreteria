4d50d5313454340594f7cf765aabfe4ed98b8195
Román <romanyaki99@gmail.com>
2025-06-02 19:41:54 -0300

    boton atras

--- FerreteriaElCosito/Proveedores.Designer.cs
+++ FerreteriaElCosito/Proveedores.Designer.cs
@@ -32,6 +32,7 @@
             this.btnagregar = new System.Windows.Forms.Button();
             this.btneditar = new System.Windows.Forms.Button();
             this.btneliminar = new System.Windows.Forms.Button();
+            this.btnatras = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvproveedores)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,12 +42,12 @@
             this.dgvproveedores.Location = new System.Drawing.Point(16, 17);
             this.dgvproveedores.Name = "dgvproveedores";
             this.dgvproveedores.RowHeadersWidth = 51;
-            this.dgvproveedores.Size = new System.Drawing.Size(647, 366);
+            this.dgvproveedores.Size = new System.Drawing.Size(647, 408);
             this.dgvproveedores.TabIndex = 0;
             // 
             // btnagregar
             // 
-            this.btnagregar.Location = new System.Drawing.Point(685, 17);
+            this.btnagregar.Location = new System.Drawing.Point(682, 17);
             this.btnagregar.Name = "btnagregar";
             this.btnagregar.Size = new System.Drawing.Size(103, 27);
             this.btnagregar.TabIndex = 1;
@@ -55,7 +56,7 @@
             // 
             // btneditar
             // 
-            this.btneditar.Location = new System.Drawing.Point(685, 60);
+            this.btneditar.Location = new System.Drawing.Point(682, 60);
             this.btneditar.Name = "btneditar";
             this.btneditar.Size = new System.Drawing.Size(103, 27);
             this.btneditar.TabIndex = 2;
@@ -64,18 +65,28 @@
             // 
             // btneliminar
             // 
-            this.btneliminar.Location = new System.Drawing.Point(685, 104);
+            this.btneliminar.Location = new System.Drawing.Point(682, 104);
             this.btneliminar.Name = "btneliminar";
             this.btneliminar.Size = new System.Drawing.Size(103, 27);
             this.btneliminar.TabIndex = 3;
             this.btneliminar.Text = "Eliminar";
             this.btneliminar.UseVisualStyleBackColor = true;
             // 
+            // btnatras
+            // 
+            this.btnatras.Location = new System.Drawing.Point(682, 398);
+            this.btnatras.Name = "btnatras";
+            this.btnatras.Size = new System.Drawing.Size(103, 27);
+            this.btnatras.TabIndex = 4;
+            this.btnatras.Text = "Atras";
+            this.btnatras.UseVisualStyleBackColor = true;
+            // 
             // Proveedores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnatras);
             this.Controls.Add(this.btneliminar);
             this.Controls.Add(this.btneditar);
             this.Controls.Add(this.btnagregar);
@@ -93,5 +104,6 @@
         private System.Windows.Forms.Button btnagregar;
         private System.Windows.Forms.Button btneditar;
         private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.Button btnatras;
     }
 }
