e918d4604a7da6a81b8414768e43a091ea7e2f2c
EmiNicora09 <eminicora2@gmail.com>
2025-06-02 19:57:52 -0300

    Diseño para agregar productos. falta codigo.

--- FerreteriaElCosito/Productos.Designer.cs
+++ FerreteriaElCosito/Productos.Designer.cs
@@ -31,9 +31,18 @@
             this.txtnombreproducto = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtdescripcionprod = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cbcategoria = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbproveedor = new System.Windows.Forms.ComboBox();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtcodigobarra = new System.Windows.Forms.TextBox();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtnombreproducto
@@ -63,43 +72,124 @@
             this.label2.Text = "Descripción:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // textBox1
+            // txtdescripcionprod
             // 
-            this.textBox1.Location = new System.Drawing.Point(150, 61);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(157, 20);
-            this.textBox1.TabIndex = 3;
+            this.txtdescripcionprod.Location = new System.Drawing.Point(150, 61);
+            this.txtdescripcionprod.Name = "txtdescripcionprod";
+            this.txtdescripcionprod.Size = new System.Drawing.Size(157, 20);
+            this.txtdescripcionprod.TabIndex = 3;
+            this.txtdescripcionprod.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(81, 117);
+            this.label3.Location = new System.Drawing.Point(92, 96);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.Size = new System.Drawing.Size(55, 13);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Código:";
+            this.label3.Text = "Categoria:";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // textBox2
+            // cbcategoria
             // 
-            this.textBox2.Location = new System.Drawing.Point(150, 114);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(157, 20);
-            this.textBox2.TabIndex = 5;
+            this.cbcategoria.FormattingEnabled = true;
+            this.cbcategoria.Location = new System.Drawing.Point(150, 93);
+            this.cbcategoria.Name = "cbcategoria";
+            this.cbcategoria.Size = new System.Drawing.Size(121, 21);
+            this.cbcategoria.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(88, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Proveedor:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // cbproveedor
+            // 
+            this.cbproveedor.FormattingEnabled = true;
+            this.cbproveedor.Location = new System.Drawing.Point(150, 129);
+            this.cbproveedor.Name = "cbproveedor";
+            this.cbproveedor.Size = new System.Drawing.Size(121, 21);
+            this.cbproveedor.TabIndex = 8;
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(150, 164);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(157, 20);
+            this.txtprecio.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(104, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Precio:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(346, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Codigo de barra:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtcodigobarra
+            // 
+            this.txtcodigobarra.Location = new System.Drawing.Point(437, 38);
+            this.txtcodigobarra.Name = "txtcodigobarra";
+            this.txtcodigobarra.Size = new System.Drawing.Size(157, 20);
+            this.txtcodigobarra.TabIndex = 12;
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Location = new System.Drawing.Point(395, 167);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(75, 23);
+            this.btnguardar.TabIndex = 13;
+            this.btnguardar.Text = "GUARDAR";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(84, 211);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(475, 86);
+            this.dataGridView1.TabIndex = 14;
             // 
             // Productos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(610, 317);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.txtcodigobarra);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtprecio);
+            this.Controls.Add(this.cbproveedor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbcategoria);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtdescripcionprod);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtnombreproducto);
             this.Name = "Productos";
             this.Text = "Productos";
+            this.Load += new System.EventHandler(this.Productos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,8 +200,16 @@
         private System.Windows.Forms.TextBox txtnombreproducto;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtdescripcionprod;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox cbcategoria;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbproveedor;
+        private System.Windows.Forms.TextBox txtprecio;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtcodigobarra;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- FerreteriaElCosito/Productos.cs
+++ FerreteriaElCosito/Productos.cs
@@ -31,5 +31,30 @@ namespace FerreteriaElCosito
         {
 
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Productos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
