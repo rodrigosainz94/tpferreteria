2692a7e3a0828e1862f95e616b1dd7a3b46e3479
Román <romanyaki99@gmail.com>
2025-06-05 18:53:49 -0300

    cambio form ProveedoresEdicion

--- FerreteriaElCosito/ProveedoresEdicion.Designer.cs
+++ FerreteriaElCosito/ProveedoresEdicion.Designer.cs
@@ -36,6 +36,18 @@
             this.lblcuitprov = new System.Windows.Forms.Label();
             this.lblprovinciaprov = new System.Windows.Forms.Label();
             this.lbllocalidadprov = new System.Windows.Forms.Label();
+            this.lblfechaaltaprov = new System.Windows.Forms.Label();
+            this.lblcativaprov = new System.Windows.Forms.Label();
+            this.txtidprov = new System.Windows.Forms.TextBox();
+            this.txtnombreprov = new System.Windows.Forms.TextBox();
+            this.txtapellidoprov = new System.Windows.Forms.TextBox();
+            this.txtemailprov = new System.Windows.Forms.TextBox();
+            this.txttelefonoprov = new System.Windows.Forms.TextBox();
+            this.txtcuitprov = new System.Windows.Forms.TextBox();
+            this.txtfechaaltaprov = new System.Windows.Forms.TextBox();
+            this.cbprovinciaprov = new System.Windows.Forms.ComboBox();
+            this.cblocalidadprov = new System.Windows.Forms.ComboBox();
+            this.txtcativaprov = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblidprov
@@ -59,7 +71,7 @@
             // lblapellidoprov
             // 
             this.lblapellidoprov.AutoSize = true;
-            this.lblapellidoprov.Location = new System.Drawing.Point(13, 64);
+            this.lblapellidoprov.Location = new System.Drawing.Point(13, 65);
             this.lblapellidoprov.Name = "lblapellidoprov";
             this.lblapellidoprov.Size = new System.Drawing.Size(60, 16);
             this.lblapellidoprov.TabIndex = 2;
@@ -68,7 +80,7 @@
             // lblemailprov
             // 
             this.lblemailprov.AutoSize = true;
-            this.lblemailprov.Location = new System.Drawing.Point(13, 89);
+            this.lblemailprov.Location = new System.Drawing.Point(13, 90);
             this.lblemailprov.Name = "lblemailprov";
             this.lblemailprov.Size = new System.Drawing.Size(44, 16);
             this.lblemailprov.TabIndex = 3;
@@ -77,7 +89,7 @@
             // lbltelefonoprov
             // 
             this.lbltelefonoprov.AutoSize = true;
-            this.lbltelefonoprov.Location = new System.Drawing.Point(13, 113);
+            this.lbltelefonoprov.Location = new System.Drawing.Point(13, 114);
             this.lbltelefonoprov.Name = "lbltelefonoprov";
             this.lbltelefonoprov.Size = new System.Drawing.Size(64, 16);
             this.lbltelefonoprov.TabIndex = 4;
@@ -86,7 +98,7 @@
             // lblcuitprov
             // 
             this.lblcuitprov.AutoSize = true;
-            this.lblcuitprov.Location = new System.Drawing.Point(13, 138);
+            this.lblcuitprov.Location = new System.Drawing.Point(13, 139);
             this.lblcuitprov.Name = "lblcuitprov";
             this.lblcuitprov.Size = new System.Drawing.Size(80, 16);
             this.lblcuitprov.TabIndex = 5;
@@ -95,7 +107,7 @@
             // lblprovinciaprov
             // 
             this.lblprovinciaprov.AutoSize = true;
-            this.lblprovinciaprov.Location = new System.Drawing.Point(13, 162);
+            this.lblprovinciaprov.Location = new System.Drawing.Point(13, 164);
             this.lblprovinciaprov.Name = "lblprovinciaprov";
             this.lblprovinciaprov.Size = new System.Drawing.Size(66, 16);
             this.lblprovinciaprov.TabIndex = 6;
@@ -104,17 +116,119 @@
             // lbllocalidadprov
             // 
             this.lbllocalidadprov.AutoSize = true;
-            this.lbllocalidadprov.Location = new System.Drawing.Point(13, 186);
+            this.lbllocalidadprov.Location = new System.Drawing.Point(13, 189);
             this.lbllocalidadprov.Name = "lbllocalidadprov";
             this.lbllocalidadprov.Size = new System.Drawing.Size(70, 16);
             this.lbllocalidadprov.TabIndex = 7;
             this.lbllocalidadprov.Text = "Localidad:";
+            // 
+            // lblfechaaltaprov
+            // 
+            this.lblfechaaltaprov.AutoSize = true;
+            this.lblfechaaltaprov.Location = new System.Drawing.Point(13, 216);
+            this.lblfechaaltaprov.Name = "lblfechaaltaprov";
+            this.lblfechaaltaprov.Size = new System.Drawing.Size(92, 16);
+            this.lblfechaaltaprov.TabIndex = 8;
+            this.lblfechaaltaprov.Text = "Fecha de alta:";
+            // 
+            // lblcativaprov
+            // 
+            this.lblcativaprov.AutoSize = true;
+            this.lblcativaprov.Location = new System.Drawing.Point(13, 242);
+            this.lblcativaprov.Name = "lblcativaprov";
+            this.lblcativaprov.Size = new System.Drawing.Size(112, 16);
+            this.lblcativaprov.TabIndex = 9;
+            this.lblcativaprov.Text = "Categoría de IVA:";
+            // 
+            // txtidprov
+            // 
+            this.txtidprov.Location = new System.Drawing.Point(171, 10);
+            this.txtidprov.Name = "txtidprov";
+            this.txtidprov.Size = new System.Drawing.Size(100, 22);
+            this.txtidprov.TabIndex = 10;
+            // 
+            // txtnombreprov
+            // 
+            this.txtnombreprov.Location = new System.Drawing.Point(171, 37);
+            this.txtnombreprov.Name = "txtnombreprov";
+            this.txtnombreprov.Size = new System.Drawing.Size(100, 22);
+            this.txtnombreprov.TabIndex = 11;
+            // 
+            // txtapellidoprov
+            // 
+            this.txtapellidoprov.Location = new System.Drawing.Point(171, 62);
+            this.txtapellidoprov.Name = "txtapellidoprov";
+            this.txtapellidoprov.Size = new System.Drawing.Size(100, 22);
+            this.txtapellidoprov.TabIndex = 12;
+            // 
+            // txtemailprov
+            // 
+            this.txtemailprov.Location = new System.Drawing.Point(171, 87);
+            this.txtemailprov.Name = "txtemailprov";
+            this.txtemailprov.Size = new System.Drawing.Size(100, 22);
+            this.txtemailprov.TabIndex = 13;
+            // 
+            // txttelefonoprov
+            // 
+            this.txttelefonoprov.Location = new System.Drawing.Point(171, 111);
+            this.txttelefonoprov.Name = "txttelefonoprov";
+            this.txttelefonoprov.Size = new System.Drawing.Size(100, 22);
+            this.txttelefonoprov.TabIndex = 14;
+            // 
+            // txtcuitprov
+            // 
+            this.txtcuitprov.Location = new System.Drawing.Point(171, 136);
+            this.txtcuitprov.Name = "txtcuitprov";
+            this.txtcuitprov.Size = new System.Drawing.Size(100, 22);
+            this.txtcuitprov.TabIndex = 15;
+            // 
+            // txtfechaaltaprov
+            // 
+            this.txtfechaaltaprov.Location = new System.Drawing.Point(171, 216);
+            this.txtfechaaltaprov.Name = "txtfechaaltaprov";
+            this.txtfechaaltaprov.Size = new System.Drawing.Size(100, 22);
+            this.txtfechaaltaprov.TabIndex = 16;
+            // 
+            // cbprovinciaprov
+            // 
+            this.cbprovinciaprov.FormattingEnabled = true;
+            this.cbprovinciaprov.Location = new System.Drawing.Point(171, 161);
+            this.cbprovinciaprov.Name = "cbprovinciaprov";
+            this.cbprovinciaprov.Size = new System.Drawing.Size(121, 24);
+            this.cbprovinciaprov.TabIndex = 17;
+            // 
+            // cblocalidadprov
+            // 
+            this.cblocalidadprov.FormattingEnabled = true;
+            this.cblocalidadprov.Location = new System.Drawing.Point(171, 189);
+            this.cblocalidadprov.Name = "cblocalidadprov";
+            this.cblocalidadprov.Size = new System.Drawing.Size(121, 24);
+            this.cblocalidadprov.TabIndex = 18;
+            // 
+            // txtcativaprov
+            // 
+            this.txtcativaprov.Location = new System.Drawing.Point(171, 242);
+            this.txtcativaprov.Name = "txtcativaprov";
+            this.txtcativaprov.Size = new System.Drawing.Size(100, 22);
+            this.txtcativaprov.TabIndex = 19;
             // 
             // ProveedoresEdicion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtcativaprov);
+            this.Controls.Add(this.cblocalidadprov);
+            this.Controls.Add(this.cbprovinciaprov);
+            this.Controls.Add(this.txtfechaaltaprov);
+            this.Controls.Add(this.txtcuitprov);
+            this.Controls.Add(this.txttelefonoprov);
+            this.Controls.Add(this.txtemailprov);
+            this.Controls.Add(this.txtapellidoprov);
+            this.Controls.Add(this.txtnombreprov);
+            this.Controls.Add(this.txtidprov);
+            this.Controls.Add(this.lblcativaprov);
+            this.Controls.Add(this.lblfechaaltaprov);
             this.Controls.Add(this.lbllocalidadprov);
             this.Controls.Add(this.lblprovinciaprov);
             this.Controls.Add(this.lblcuitprov);
@@ -140,5 +254,17 @@
         private System.Windows.Forms.Label lblcuitprov;
         private System.Windows.Forms.Label lblprovinciaprov;
         private System.Windows.Forms.Label lbllocalidadprov;
+        private System.Windows.Forms.Label lblfechaaltaprov;
+        private System.Windows.Forms.Label lblcativaprov;
+        private System.Windows.Forms.TextBox txtidprov;
+        private System.Windows.Forms.TextBox txtnombreprov;
+        private System.Windows.Forms.TextBox txtapellidoprov;
+        private System.Windows.Forms.TextBox txtemailprov;
+        private System.Windows.Forms.TextBox txttelefonoprov;
+        private System.Windows.Forms.TextBox txtcuitprov;
+        private System.Windows.Forms.TextBox txtfechaaltaprov;
+        private System.Windows.Forms.ComboBox cbprovinciaprov;
+        private System.Windows.Forms.ComboBox cblocalidadprov;
+        private System.Windows.Forms.TextBox txtcativaprov;
     }
 }
