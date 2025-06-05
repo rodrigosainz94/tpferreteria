6008d451538e221e18f26a021bb2b85411f8d282
F Marin <marinfabiana@gmail.com>
2025-06-05 12:03:02 -0300

    generacion del formulario roles

---
+++ FerreteriaElCosito/Roles.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace FerreteriaElCosito
+{
+    partial class Roles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtnombrerol = new System.Windows.Forms.TextBox();
+            this.lblnombrerol = new System.Windows.Forms.Label();
+            this.txtdescripcioniva = new System.Windows.Forms.TextBox();
+            this.lbldescripcionrol = new System.Windows.Forms.Label();
+            this.txtidrol = new System.Windows.Forms.TextBox();
+            this.lblidrol = new System.Windows.Forms.Label();
+            this.lblvolveralinicio = new System.Windows.Forms.Label();
+            this.lblalta = new System.Windows.Forms.Label();
+            this.lblbaja = new System.Windows.Forms.Label();
+            this.lblmodificacion = new System.Windows.Forms.Label();
+            this.lblconsulta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtnombrerol
+            // 
+            this.txtnombrerol.Location = new System.Drawing.Point(203, 109);
+            this.txtnombrerol.Name = "txtnombrerol";
+            this.txtnombrerol.Size = new System.Drawing.Size(128, 22);
+            this.txtnombrerol.TabIndex = 63;
+            // 
+            // lblnombrerol
+            // 
+            this.lblnombrerol.AutoSize = true;
+            this.lblnombrerol.Location = new System.Drawing.Point(83, 116);
+            this.lblnombrerol.Name = "lblnombrerol";
+            this.lblnombrerol.Size = new System.Drawing.Size(80, 16);
+            this.lblnombrerol.TabIndex = 62;
+            this.lblnombrerol.Text = "Nombre Rol";
+            // 
+            // txtdescripcioniva
+            // 
+            this.txtdescripcioniva.Location = new System.Drawing.Point(203, 165);
+            this.txtdescripcioniva.Multiline = true;
+            this.txtdescripcioniva.Name = "txtdescripcioniva";
+            this.txtdescripcioniva.Size = new System.Drawing.Size(315, 144);
+            this.txtdescripcioniva.TabIndex = 61;
+            // 
+            // lbldescripcionrol
+            // 
+            this.lbldescripcionrol.AutoSize = true;
+            this.lbldescripcionrol.Location = new System.Drawing.Point(83, 172);
+            this.lbldescripcionrol.Name = "lbldescripcionrol";
+            this.lbldescripcionrol.Size = new System.Drawing.Size(103, 16);
+            this.lbldescripcionrol.TabIndex = 60;
+            this.lbldescripcionrol.Text = "Descripcion Rol";
+            // 
+            // txtidrol
+            // 
+            this.txtidrol.Location = new System.Drawing.Point(203, 59);
+            this.txtidrol.Name = "txtidrol";
+            this.txtidrol.Size = new System.Drawing.Size(128, 22);
+            this.txtidrol.TabIndex = 59;
+            // 
+            // lblidrol
+            // 
+            this.lblidrol.AutoSize = true;
+            this.lblidrol.Location = new System.Drawing.Point(83, 66);
+            this.lblidrol.Name = "lblidrol";
+            this.lblidrol.Size = new System.Drawing.Size(42, 16);
+            this.lblidrol.TabIndex = 58;
+            this.lblidrol.Text = "Id Rol";
+            // 
+            // lblvolveralinicio
+            // 
+            this.lblvolveralinicio.AutoSize = true;
+            this.lblvolveralinicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblvolveralinicio.Location = new System.Drawing.Point(629, 407);
+            this.lblvolveralinicio.Name = "lblvolveralinicio";
+            this.lblvolveralinicio.Size = new System.Drawing.Size(134, 25);
+            this.lblvolveralinicio.TabIndex = 57;
+            this.lblvolveralinicio.Text = "Volver a Inicio";
+            // 
+            // lblalta
+            // 
+            this.lblalta.AutoSize = true;
+            this.lblalta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblalta.Location = new System.Drawing.Point(137, 350);
+            this.lblalta.Name = "lblalta";
+            this.lblalta.Size = new System.Drawing.Size(41, 22);
+            this.lblalta.TabIndex = 56;
+            this.lblalta.Text = "Alta";
+            // 
+            // lblbaja
+            // 
+            this.lblbaja.AutoSize = true;
+            this.lblbaja.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblbaja.Location = new System.Drawing.Point(218, 350);
+            this.lblbaja.Name = "lblbaja";
+            this.lblbaja.Size = new System.Drawing.Size(46, 22);
+            this.lblbaja.TabIndex = 55;
+            this.lblbaja.Text = "Baja";
+            // 
+            // lblmodificacion
+            // 
+            this.lblmodificacion.AutoSize = true;
+            this.lblmodificacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmodificacion.Location = new System.Drawing.Point(326, 350);
+            this.lblmodificacion.Name = "lblmodificacion";
+            this.lblmodificacion.Size = new System.Drawing.Size(109, 22);
+            this.lblmodificacion.TabIndex = 54;
+            this.lblmodificacion.Text = "Modificacion";
+            // 
+            // lblconsulta
+            // 
+            this.lblconsulta.AutoSize = true;
+            this.lblconsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblconsulta.Location = new System.Drawing.Point(23, 350);
+            this.lblconsulta.Name = "lblconsulta";
+            this.lblconsulta.Size = new System.Drawing.Size(81, 22);
+            this.lblconsulta.TabIndex = 53;
+            this.lblconsulta.Text = "Consulta";
+            // 
+            // Roles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtnombrerol);
+            this.Controls.Add(this.lblnombrerol);
+            this.Controls.Add(this.txtdescripcioniva);
+            this.Controls.Add(this.lbldescripcionrol);
+            this.Controls.Add(this.txtidrol);
+            this.Controls.Add(this.lblidrol);
+            this.Controls.Add(this.lblvolveralinicio);
+            this.Controls.Add(this.lblalta);
+            this.Controls.Add(this.lblbaja);
+            this.Controls.Add(this.lblmodificacion);
+            this.Controls.Add(this.lblconsulta);
+            this.Name = "Roles";
+            this.Text = "Roles";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtnombrerol;
+        private System.Windows.Forms.Label lblnombrerol;
+        private System.Windows.Forms.TextBox txtdescripcioniva;
+        private System.Windows.Forms.Label lbldescripcionrol;
+        private System.Windows.Forms.TextBox txtidrol;
+        private System.Windows.Forms.Label lblidrol;
+        private System.Windows.Forms.Label lblvolveralinicio;
+        private System.Windows.Forms.Label lblalta;
+        private System.Windows.Forms.Label lblbaja;
+        private System.Windows.Forms.Label lblmodificacion;
+        private System.Windows.Forms.Label lblconsulta;
+    }
+}
