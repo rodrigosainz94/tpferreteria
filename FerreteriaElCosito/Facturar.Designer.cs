d4aca8cfe00efb668aad526ce18f576c6d27aed9
Román <romanyaki99@gmail.com>
2025-08-13 12:15:00 -0300

    Actualiza form Facturar

--- FerreteriaElCosito/Facturar.Designer.cs
+++ FerreteriaElCosito/Facturar.Designer.cs
@@ -34,7 +34,6 @@
             this.txtcodigo = new System.Windows.Forms.TextBox();
             this.lblcodigo = new System.Windows.Forms.Label();
             this.lblnombre = new System.Windows.Forms.Label();
-            this.txtnombre = new System.Windows.Forms.TextBox();
             this.btnagregar = new System.Windows.Forms.Button();
             this.btneliminar = new System.Windows.Forms.Button();
             this.txttotal = new System.Windows.Forms.TextBox();
@@ -46,90 +45,98 @@
             this.txtdni = new System.Windows.Forms.TextBox();
             this.cbconsumidorfinal = new System.Windows.Forms.CheckBox();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.cmbnombre = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvfacturacion)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvfacturacion
             // 
             this.dgvfacturacion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvfacturacion.Location = new System.Drawing.Point(295, 46);
+            this.dgvfacturacion.Location = new System.Drawing.Point(393, 57);
+            this.dgvfacturacion.Margin = new System.Windows.Forms.Padding(4);
             this.dgvfacturacion.Name = "dgvfacturacion";
-            this.dgvfacturacion.Size = new System.Drawing.Size(456, 366);
+            this.dgvfacturacion.RowHeadersWidth = 51;
+            this.dgvfacturacion.Size = new System.Drawing.Size(608, 450);
             this.dgvfacturacion.TabIndex = 0;
-            this.dgvfacturacion.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvfacturacion_CellContentClick);
+            this.dgvfacturacion.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvfacturacion_CellEndEdit);
+            this.dgvfacturacion.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dgvfacturacion_KeyDown);
             // 
             // lblfecha
             // 
             this.lblfecha.AutoSize = true;
-            this.lblfecha.Location = new System.Drawing.Point(643, 18);
+            this.lblfecha.Location = new System.Drawing.Point(857, 22);
+            this.lblfecha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblfecha.Name = "lblfecha";
-            this.lblfecha.Size = new System.Drawing.Size(108, 13);
+            this.lblfecha.Size = new System.Drawing.Size(126, 16);
             this.lblfecha.TabIndex = 1;
             this.lblfecha.Text = "hora y fecha : 1 2 3 4";
             // 
             // txtcodigo
             // 
-            this.txtcodigo.Location = new System.Drawing.Point(128, 95);
+            this.txtcodigo.Location = new System.Drawing.Point(168, 123);
+            this.txtcodigo.Margin = new System.Windows.Forms.Padding(4);
             this.txtcodigo.Name = "txtcodigo";
-            this.txtcodigo.Size = new System.Drawing.Size(138, 20);
+            this.txtcodigo.Size = new System.Drawing.Size(183, 22);
             this.txtcodigo.TabIndex = 2;
+            this.txtcodigo.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtcodigo_KeyDown);
             // 
             // lblcodigo
             // 
             this.lblcodigo.AutoSize = true;
-            this.lblcodigo.Location = new System.Drawing.Point(67, 102);
+            this.lblcodigo.Location = new System.Drawing.Point(89, 126);
+            this.lblcodigo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblcodigo.Name = "lblcodigo";
-            this.lblcodigo.Size = new System.Drawing.Size(55, 13);
+            this.lblcodigo.Size = new System.Drawing.Size(65, 16);
             this.lblcodigo.TabIndex = 3;
             this.lblcodigo.Text = "CÓDIGO :";
             // 
             // lblnombre
             // 
             this.lblnombre.AutoSize = true;
-            this.lblnombre.Location = new System.Drawing.Point(67, 128);
+            this.lblnombre.Location = new System.Drawing.Point(89, 158);
+            this.lblnombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblnombre.Name = "lblnombre";
-            this.lblnombre.Size = new System.Drawing.Size(60, 13);
+            this.lblnombre.Size = new System.Drawing.Size(72, 16);
             this.lblnombre.TabIndex = 5;
             this.lblnombre.Text = "NOMBRE :";
             // 
-            // txtnombre
-            // 
-            this.txtnombre.Location = new System.Drawing.Point(128, 121);
-            this.txtnombre.Name = "txtnombre";
-            this.txtnombre.Size = new System.Drawing.Size(138, 20);
-            this.txtnombre.TabIndex = 4;
-            // 
             // btnagregar
             // 
-            this.btnagregar.Location = new System.Drawing.Point(70, 164);
+            this.btnagregar.Location = new System.Drawing.Point(93, 202);
+            this.btnagregar.Margin = new System.Windows.Forms.Padding(4);
             this.btnagregar.Name = "btnagregar";
-            this.btnagregar.Size = new System.Drawing.Size(75, 23);
+            this.btnagregar.Size = new System.Drawing.Size(100, 28);
             this.btnagregar.TabIndex = 6;
             this.btnagregar.Text = "AGREGAR";
             this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
             // 
             // btneliminar
             // 
-            this.btneliminar.Location = new System.Drawing.Point(191, 164);
+            this.btneliminar.Location = new System.Drawing.Point(255, 202);
+            this.btneliminar.Margin = new System.Windows.Forms.Padding(4);
             this.btneliminar.Name = "btneliminar";
-            this.btneliminar.Size = new System.Drawing.Size(75, 23);
+            this.btneliminar.Size = new System.Drawing.Size(100, 28);
             this.btneliminar.TabIndex = 7;
             this.btneliminar.Text = "ELIMINAR";
             this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
             // 
             // txttotal
             // 
-            this.txttotal.Location = new System.Drawing.Point(651, 418);
+            this.txttotal.Location = new System.Drawing.Point(868, 514);
+            this.txttotal.Margin = new System.Windows.Forms.Padding(4);
             this.txttotal.Name = "txttotal";
-            this.txttotal.Size = new System.Drawing.Size(100, 20);
+            this.txttotal.Size = new System.Drawing.Size(132, 22);
             this.txttotal.TabIndex = 8;
             // 
             // lbltotal
             // 
             this.lbltotal.AutoSize = true;
-            this.lbltotal.Location = new System.Drawing.Point(597, 425);
+            this.lbltotal.Location = new System.Drawing.Point(796, 523);
+            this.lbltotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbltotal.Name = "lbltotal";
-            this.lbltotal.Size = new System.Drawing.Size(48, 13);
+            this.lbltotal.Size = new System.Drawing.Size(57, 16);
             this.lbltotal.TabIndex = 9;
             this.lbltotal.Text = "TOTAL :";
             // 
@@ -138,9 +145,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(101, 18);
+            this.label1.Location = new System.Drawing.Point(135, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(134, 22);
+            this.label1.Size = new System.Drawing.Size(169, 29);
             this.label1.TabIndex = 10;
             this.label1.Text = "FERRETERIA";
             // 
@@ -149,17 +157,19 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label2.Location = new System.Drawing.Point(101, 46);
+            this.label2.Location = new System.Drawing.Point(135, 57);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(140, 22);
+            this.label2.Size = new System.Drawing.Size(172, 29);
             this.label2.TabIndex = 11;
             this.label2.Text = "\" EL COSITO \"";
             // 
             // btnatras
             // 
-            this.btnatras.Location = new System.Drawing.Point(24, 415);
+            this.btnatras.Location = new System.Drawing.Point(32, 511);
+            this.btnatras.Margin = new System.Windows.Forms.Padding(4);
             this.btnatras.Name = "btnatras";
-            this.btnatras.Size = new System.Drawing.Size(75, 23);
+            this.btnatras.Size = new System.Drawing.Size(100, 28);
             this.btnatras.TabIndex = 12;
             this.btnatras.Text = "ATRAS";
             this.btnatras.UseVisualStyleBackColor = true;
@@ -168,35 +178,48 @@
             // lbldni
             // 
             this.lbldni.AutoSize = true;
-            this.lbldni.Location = new System.Drawing.Point(67, 218);
+            this.lbldni.Location = new System.Drawing.Point(89, 268);
+            this.lbldni.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbldni.Name = "lbldni";
-            this.lbldni.Size = new System.Drawing.Size(32, 13);
+            this.lbldni.Size = new System.Drawing.Size(36, 16);
             this.lbldni.TabIndex = 13;
             this.lbldni.Text = "DNI :";
             // 
             // txtdni
             // 
-            this.txtdni.Location = new System.Drawing.Point(128, 211);
+            this.txtdni.Location = new System.Drawing.Point(171, 260);
+            this.txtdni.Margin = new System.Windows.Forms.Padding(4);
             this.txtdni.Name = "txtdni";
-            this.txtdni.Size = new System.Drawing.Size(138, 20);
+            this.txtdni.Size = new System.Drawing.Size(183, 22);
             this.txtdni.TabIndex = 14;
             // 
             // cbconsumidorfinal
             // 
             this.cbconsumidorfinal.AutoSize = true;
-            this.cbconsumidorfinal.Location = new System.Drawing.Point(70, 237);
+            this.cbconsumidorfinal.Location = new System.Drawing.Point(93, 292);
+            this.cbconsumidorfinal.Margin = new System.Windows.Forms.Padding(4);
             this.cbconsumidorfinal.Name = "cbconsumidorfinal";
-            this.cbconsumidorfinal.Size = new System.Drawing.Size(133, 17);
+            this.cbconsumidorfinal.Size = new System.Drawing.Size(161, 20);
             this.cbconsumidorfinal.TabIndex = 15;
             this.cbconsumidorfinal.Text = "CONSUMIDOR FINAL";
             this.cbconsumidorfinal.UseVisualStyleBackColor = true;
             // 
+            // cmbnombre
+            // 
+            this.cmbnombre.FormattingEnabled = true;
+            this.cmbnombre.Location = new System.Drawing.Point(168, 155);
+            this.cmbnombre.Name = "cmbnombre";
+            this.cmbnombre.Size = new System.Drawing.Size(183, 24);
+            this.cmbnombre.TabIndex = 16;
+            this.cmbnombre.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cmbnombre_KeyDown);
+            // 
             // Facturar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.cmbnombre);
             this.Controls.Add(this.cbconsumidorfinal);
             this.Controls.Add(this.txtdni);
             this.Controls.Add(this.lbldni);
@@ -208,12 +231,12 @@
             this.Controls.Add(this.btneliminar);
             this.Controls.Add(this.btnagregar);
             this.Controls.Add(this.lblnombre);
-            this.Controls.Add(this.txtnombre);
             this.Controls.Add(this.lblcodigo);
             this.Controls.Add(this.txtcodigo);
             this.Controls.Add(this.lblfecha);
             this.Controls.Add(this.dgvfacturacion);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Facturar";
             this.Text = "Facturar";
             this.Load += new System.EventHandler(this.Facturar_Load);
@@ -230,7 +253,6 @@
         private System.Windows.Forms.TextBox txtcodigo;
         private System.Windows.Forms.Label lblcodigo;
         private System.Windows.Forms.Label lblnombre;
-        private System.Windows.Forms.TextBox txtnombre;
         private System.Windows.Forms.Button btnagregar;
         private System.Windows.Forms.Button btneliminar;
         private System.Windows.Forms.TextBox txttotal;
@@ -242,5 +264,6 @@
         private System.Windows.Forms.TextBox txtdni;
         private System.Windows.Forms.CheckBox cbconsumidorfinal;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox cmbnombre;
     }
 }
--- FerreteriaElCosito/Facturar.cs
+++ FerreteriaElCosito/Facturar.cs
@@ -1,18 +1,15 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FerreteriaElCosito
 {
     public partial class Facturar : Form
     {
+        private DataTable dtFacturaItems = new DataTable();
+
         public Facturar()
         {
             InitializeComponent();
@@ -20,40 +17,200 @@ namespace FerreteriaElCosito
 
         private void Facturar_Load(object sender, EventArgs e)
         {
-            CargarDatos();
+            ConfigurarGrillaFactura();
+            CargarComboProductos(); // Cargamos el nuevo ComboBox
+
+            // Dejamos los campos de entrada listos
+            lblfecha.Text = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+            cmbnombre.SelectedIndex = -1;
+            this.ActiveControl = txtcodigo;
         }
 
-        private void CargarDatos()
+        private void ConfigurarGrillaFactura()
+        {
+            dgvfacturacion.AutoGenerateColumns = false;
+
+            dtFacturaItems.Columns.Add("IdProducto", typeof(int));
+            dtFacturaItems.Columns.Add("Nombre", typeof(string));
+            dtFacturaItems.Columns.Add("Cantidad", typeof(int));
+            dtFacturaItems.Columns.Add("Medida", typeof(string));
+            dtFacturaItems.Columns.Add("Precio unidad", typeof(decimal));
+            dtFacturaItems.Columns.Add("Precio cantidad", typeof(decimal), "[Precio unidad] * Cantidad");
+
+            dgvfacturacion.DataSource = dtFacturaItems;
+
+            if (dgvfacturacion.Columns.Count == 0)
+            {
+                var colNombre = new DataGridViewTextBoxColumn { Name = "Nombre", HeaderText = "Nombre", DataPropertyName = "Nombre", ReadOnly = true, Width = 200 };
+                var colCantidad = new DataGridViewTextBoxColumn { Name = "Cantidad", HeaderText = "Cantidad", DataPropertyName = "Cantidad", ReadOnly = false };
+                var colMedida = new DataGridViewTextBoxColumn { Name = "Medida", HeaderText = "Medida", DataPropertyName = "Medida", ReadOnly = true, Width = 80 };
+                var colPrecioUnidad = new DataGridViewTextBoxColumn { Name = "PrecioUnidad", HeaderText = "Precio Unidad", DataPropertyName = "Precio unidad", ReadOnly = true, DefaultCellStyle = { Format = "C" } };
+                var colPrecioCantidad = new DataGridViewTextBoxColumn { Name = "PrecioCantidad", HeaderText = "Precio Cantidad", DataPropertyName = "Precio cantidad", ReadOnly = true, DefaultCellStyle = { Format = "C" } };
+
+                dgvfacturacion.Columns.AddRange(new DataGridViewColumn[] { colNombre, colCantidad, colMedida, colPrecioUnidad, colPrecioCantidad });
+            }
+
+            dgvfacturacion.AllowUserToAddRows = false;
+        }
+
+        /// <summary>
+        /// NUEVO: Carga los productos en el ComboBox de nombres.
+        /// </summary>
+        private void CargarComboProductos()
         {
             try
             {
                 using (MySqlConnection conn = ConexionBD.ObtenerConexion())
                 {
-                    string query = "SELECT * FROM productos";
-                    MySqlCommand cmd = new MySqlCommand(query, conn);
-                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-                    DataTable dt = new DataTable();
-                    da.Fill(dt);
+                    string query = "SELECT IdProducto, NombreProducto FROM productos ORDER BY NombreProducto";
+                    MySqlDataAdapter da = new MySqlDataAdapter(query, conn);
+                    DataTable dtProductos = new DataTable();
+                    da.Fill(dtProductos);
 
-                    dgvfacturacion.DataSource = dt;
+                    cmbnombre.DataSource = dtProductos;
+                    cmbnombre.DisplayMember = "NombreProducto";
+                    cmbnombre.ValueMember = "IdProducto";
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error: " + ex.Message);
+                MessageBox.Show("Error al cargar la lista de productos: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// MODIFICADO: Ahora maneja tanto el código como el ComboBox.
+        /// </summary>
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            int idProductoBuscado = 0;
+
+            if (!string.IsNullOrWhiteSpace(txtcodigo.Text))
+            {
+                if (!int.TryParse(txtcodigo.Text, out idProductoBuscado))
+                {
+                    MessageBox.Show("El código de producto debe ser un número válido.");
+                    return;
+                }
+            }
+            else if (cmbnombre.SelectedValue != null)
+            {
+                idProductoBuscado = Convert.ToInt32(cmbnombre.SelectedValue);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un código o seleccione un producto de la lista.");
+                return;
+            }
+
+            DataRow filaExistente = dtFacturaItems.AsEnumerable().FirstOrDefault(row => (int)row["IdProducto"] == idProductoBuscado);
+            if (filaExistente != null)
+            {
+                filaExistente["Cantidad"] = (int)filaExistente["Cantidad"] + 1;
+            }
+            else
+            {
+                try
+                {
+                    using (MySqlConnection conn = ConexionBD.ObtenerConexion())
+                    {
+                        string query = "SELECT NombreProducto, UnidadMedida, PrecioUnitario FROM productos WHERE IdProducto = @id";
+                        MySqlCommand cmd = new MySqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("@id", idProductoBuscado);
+
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                dtFacturaItems.Rows.Add(idProductoBuscado, reader["NombreProducto"].ToString(), 1, reader["UnidadMedida"].ToString(), Convert.ToDecimal(reader["PrecioUnitario"]));
+                            }
+                            else
+                            {
+                                MessageBox.Show("Producto no encontrado.");
+                                return;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al agregar producto: " + ex.Message);
+                    return;
+                }
+            }
+
+            CalcularTotal();
+            txtcodigo.Clear();
+            cmbnombre.SelectedIndex = -1;
+            txtcodigo.Focus();
+        }
+
+        // --- MANEJO DE TECLADO ---
+
+        private void txtcodigo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                btnagregar.PerformClick();
+            }
+        }
+
+        /// <summary>
+        /// NUEVO: Maneja la tecla Enter para el ComboBox.
+        /// </summary>
+        private void cmbnombre_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                btnagregar.PerformClick();
+            }
+        }
+
+        private void dgvfacturacion_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && dgvfacturacion.CurrentCell != null && dgvfacturacion.CurrentCell.OwningColumn.Name == "Cantidad")
+            {
+                dgvfacturacion.EndEdit();
+                e.Handled = true;
+                txtcodigo.Focus();
+            }
+        }
+
+        private void dgvfacturacion_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            CalcularTotal();
+        }
+
+        // --- MÉTODOS AUXILIARES Y OTROS BOTONES ---
+
+        private void CalcularTotal()
+        {
+            decimal total = 0;
+            if (dtFacturaItems.Rows.Count > 0)
+            {
+                total = Convert.ToDecimal(dtFacturaItems.Compute("SUM([Precio cantidad])", string.Empty));
+            }
+            txttotal.Text = total.ToString("C");
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvfacturacion.CurrentRow != null && !dgvfacturacion.CurrentRow.IsNewRow)
+            {
+                dgvfacturacion.Rows.Remove(dgvfacturacion.CurrentRow);
+                CalcularTotal();
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un producto de la lista para eliminar.");
             }
         }
 
         private void btnatras_Click(object sender, EventArgs e)
         {
-            Menuprincipal nuevoFormulario = new Menuprincipal();
-            nuevoFormulario.Show();
-            this.Hide();
-        }
-
-        private void dgvfacturacion_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            // Opcional: código si querés hacer algo al hacer clic en una celda
+            this.Close();
         }
     }
 }
